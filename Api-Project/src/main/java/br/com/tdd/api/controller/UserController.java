f3da30a7397e578c75e5d0cfc9725e1885b87fe7
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2022-10-04 17:46:41 -0300

    DTO pattern 🐱‍💻

--- Api-Project/src/main/java/br/com/tdd/api/controller/UserController.java
+++ Api-Project/src/main/java/br/com/tdd/api/controller/UserController.java
@@ -1,5 +1,6 @@
 package br.com.tdd.api.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.tdd.api.entity.UserEntity;
+import br.com.tdd.api.dto.UserDTO;
 import br.com.tdd.api.services.UserService;
 
 @RestController
@@ -15,11 +16,14 @@ import br.com.tdd.api.services.UserService;
 public class UserController {
 	
 	@Autowired
+	private ModelMapper mapper;
+	
+	@Autowired
 	private UserService userService;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<UserEntity> findById(@PathVariable long id) {
-		return ResponseEntity.ok().body(userService.findById(id));
+	public ResponseEntity<UserDTO> findById(@PathVariable long id) {
+		return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
 	}
 	
 }
