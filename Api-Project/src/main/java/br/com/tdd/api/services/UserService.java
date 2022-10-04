a82debd2e23b11228ce7aec2d5c24a05608b601c
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2022-10-04 12:37:08 -0300

    user service 🐱‍💻

---
+++ Api-Project/src/main/java/br/com/tdd/api/services/UserService.java
@@ -0,0 +1,5 @@
+package br.com.tdd.api.services;
+
+public interface UserService {
+
+}
---
+++ Api-Project/src/main/java/br/com/tdd/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package br.com.tdd.api.services.impl;
+
+import br.com.tdd.api.entity.UserEntity;
+import br.com.tdd.api.services.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserEntity findById(long id) {
+		return null;
+	}
+
+}
